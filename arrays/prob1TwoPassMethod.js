3ac4bd6c2be86f81fb5cd70e05400fc1baa1ab12
Suresh <shansuresh25@gmail.com>
2024-11-20 23:54:56 +0530

    find second max in array using two pass method

---
+++ arrays/prob1TwoPassMethod.js
@@ -0,0 +1,22 @@
+function getSecondLargestTwoPass(arr){
+
+    let firstMax = -1;
+    let secondMax = -1;
+
+    // find the first largest through first traversal
+    for(let i = 0; i < arr.length;i++){
+        if(arr[i] > firstMax){
+            firstMax = arr[i];
+        }
+    }
+
+    // find the second max through the second traversal
+    for(let j = 0; j < arr.length; j++){
+        
+        if(arr[j] > secondMax && arr[j] !== firstMax){
+            secondMax = arr[j];
+        }
+    }
+
+    return secondMax;
+}
